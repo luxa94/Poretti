2fc42d6d7aa8ca75aa5a16cf8b66c830bd9547bb
Jasna Stankovic <jaca994@yahoo.com>
2016-10-15 13:05:41 +0200

    Initial commit

---
+++ src/main/java/com/bdzjn/poretti/PorettiApplication.java
@@ -0,0 +1,12 @@
+package com.bdzjn.poretti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PorettiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PorettiApplication.class, args);
+	}
+}
