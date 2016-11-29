17edc4ac5d47fd5b748167700536ad5bea3303ab
Jasna Stankovic <jaca994@yahoo.com>
2016-11-29 21:47:59 +0100

    Added tests for creating admin

--- src/test/java/com/bdzjn/poretti/PorettiApplicationTests.java
+++ src/test/java/com/bdzjn/poretti/PorettiApplicationTests.java
@@ -2,11 +2,14 @@ package com.bdzjn.poretti;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@EnableAutoConfiguration
 public class PorettiApplicationTests {
 
 	@Test
--- src/test/java/com/bdzjn/poretti/integration/UserControllerIntegrationTest.java
+++ src/test/java/com/bdzjn/poretti/integration/UserControllerIntegrationTest.java
@@ -1,41 +1,83 @@
 package com.bdzjn.poretti.integration;
 
 import com.bdzjn.poretti.controller.dto.RegisterDTO;
+import com.bdzjn.poretti.util.TestUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
+@AutoConfigureMockMvc
 @ActiveProfiles(profiles = "test")
 public class UserControllerIntegrationTest {
 
-    private static final String USERS_BASE_URL = "/api/users";
+    private static final String URL_PREFIX = "/api/users";
+    private static final String AUTHORIZATION = "X-AUTH-TOKEN";
+    private static final String TOKEN_VALUE = "102da414-847d-4602-8b2d-edca26ab26d7";
 
     @Autowired
-    TestRestTemplate restTemplate;
+    MockMvc mockMvc;
 
    @Test
-    public void createAdminShouldReturnCreatedWhenUsernameOrEmailNotTaken(){
-        final String createAdminUrl = USERS_BASE_URL + "/createSysAdmin";
-        final RegisterDTO testAdmin = testAdmin();
-        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(createAdminUrl, testAdmin, Long.class);
+   @Transactional
+   public void createAdminShouldReturnCreatedWhenUsernameOrEmailAreNotTaken() throws Exception{
+       final String CREATE_ADMIN_URL = URL_PREFIX + "/createSysAdmin";
+       final RegisterDTO testAdmin = testEntityForCreateAdmin();
 
-        assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+       this.mockMvc.perform(MockMvcRequestBuilders.post(CREATE_ADMIN_URL)
+               .header(AUTHORIZATION, TOKEN_VALUE)
+               .contentType(MediaType.APPLICATION_JSON)
+               .content(TestUtil.json(testAdmin)))
+               .andExpect(status().isCreated());
    }
 
+    @Test
+    @Transactional
+    public void createAdminShouldReturnWhenUnprocessableWhenUsernameIsTaken() throws Exception{
+        final String CREATE_ADMIN_URL = URL_PREFIX + "/createSysAdmin";
 
-    private RegisterDTO testAdmin(){
-       RegisterDTO registerAdmin = new RegisterDTO();
-       registerAdmin.setEmail("admin@admin.com");
+        final RegisterDTO testAdminWithExistingUsername = testEntityForCreateAdmin();
+        testAdminWithExistingUsername.setUsername("admin");
+
+        this.mockMvc.perform(MockMvcRequestBuilders.post(CREATE_ADMIN_URL)
+                .header(AUTHORIZATION, TOKEN_VALUE)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(TestUtil.json(testAdminWithExistingUsername)))
+                .andExpect(status().isUnprocessableEntity())
+                .andExpect(content().string("Username or email taken."));
+    }
+
+    @Test
+    @Transactional
+    public void createAdminShouldReturnWhenUnprecessableWhenEmailIsTaken() throws Exception{
+        final String CREATE_ADMIN_URL = URL_PREFIX + "/createSysAdmin";
+
+        final RegisterDTO testAdminWithExistingEmail = testEntityForCreateAdmin();
+        testAdminWithExistingEmail.setEmail("ja");
+
+        this.mockMvc.perform(MockMvcRequestBuilders.post(CREATE_ADMIN_URL)
+                .header(AUTHORIZATION, TOKEN_VALUE)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(TestUtil.json(testAdminWithExistingEmail)))
+                .andExpect(status().isUnprocessableEntity())
+                .andExpect(content().string("Username or email taken."));
+    }
+
+    private RegisterDTO testEntityForCreateAdmin(){
+       final RegisterDTO registerAdmin = new RegisterDTO();
+       registerAdmin.setEmail("newadmin@admin.com");
        registerAdmin.setName("Admin Senior");
        registerAdmin.setUsername("admin_senior");
        registerAdmin.setPassword("admin_senior");
