70c22dc8f186329a1c2b58a8f0ebd4dea5fb86a8
Jasna Stankovic <jaca994@yahoo.com>
2017-01-17 20:44:27 +0100

    Fixed tests from admin.

--- src/test/protractor/e2e/users/admin/admin.pageObject.js
+++ src/test/protractor/e2e/users/admin/admin.pageObject.js
@@ -1,48 +1,49 @@
-var AdminPage = function() {};
+var AdminPage = function () {
+};
 
 AdminPage.prototype = Object.create({}, {
 
     newUserUsernameInput: {
-        get : function() {
+        get: function () {
             return element(by.id("admin-input-newUser-username"));
         },
-        set: function(value) {
+        set: function (value) {
             return this.newUserUsernameInput.clear().sendKeys(value);
         }
     },
 
     newUserPasswordInput: {
-        get : function() {
+        get: function () {
             return element(by.id("admin-input-newUser-password"));
         },
-        set: function(value) {
+        set: function (value) {
             return this.newUserPasswordInput.clear().sendKeys(value);
         }
     },
 
     newUserEmailInput: {
-        get : function() {
+        get: function () {
             return element(by.id("admin-input-newUser-email"));
         },
-        set: function(value) {
+        set: function (value) {
             return this.newUserEmailInput.clear().sendKeys(value);
         }
     },
 
     newUserNameInput: {
-        get : function() {
+        get: function () {
             return element(by.id("admin-input-newUser-name"));
         },
-        set: function(value) {
+        set: function (value) {
             return this.newUserNameInput.clear().sendKeys(value);
         }
     },
 
     newUserRoleSelect: {
-        get : function() {
+        get: function () {
             return element(by.id("admin-select-newUser-role"));
         },
-        set: function(value) {
+        set: function (value) {
             return this.newUserRoleSelect.sendKeys(value);
         }
     },
@@ -50,6 +51,165 @@ AdminPage.prototype = Object.create({}, {
     createAdminVerifierButton: {
         get: function () {
             return element(by.id("admin-btn-createAdminVerifier"));
+        }
+    },
+
+    companyPibInput: {
+        get: function () {
+            return element(by.id("admin-input-company-pib"));
+        },
+        set: function (value) {
+            return this.companyPibInput.clear().sendKeys(value);
+        }
+    },
+
+    companyNameInput: {
+        get: function () {
+            return element(by.id("admin-input-company-name"));
+        },
+        set: function (value) {
+            return this.companyNameInput.clear().sendKeys(value);
+        }
+    },
+
+    addCompanyUserButton: {
+        get: function () {
+            return element(by.id("admin-btn-company-addUser"));
+        }
+    },
+
+    addedCompanyUserSpan: {
+        get: function () {
+            return element(by.id("admin-span-addedUser"));
+        }
+    },
+
+    createCompanyButton: {
+        get: function () {
+            return element(by.id("admin-btn-createCompany"));
+        }
+    },
+
+    locationStateInput: {
+        get: function () {
+            return element(by.id("admin-input-location-state"));
+        },
+        set: function (value) {
+            return this.locationStateInput.clear().sendKeys(value);
+        }
+    },
+
+    locationCityInput: {
+        get: function () {
+            return element(by.id("admin-input-location-city"));
+        },
+        set: function (value) {
+            return this.locationCityInput.clear().sendKeys(value);
+        }
+    },
+
+    locationCityAreaInput: {
+        get: function () {
+            return element(by.id("admin-input-location-cityArea"));
+        },
+        set: function (value) {
+            return this.locationCityAreaInput.clear().sendKeys(value);
+        }
+    },
+
+    locationZipCodeInput: {
+        get: function () {
+            return element(by.id("admin-input-location-zipCode"));
+        },
+        set: function (value) {
+            return this.locationZipCodeInput.clear().sendKeys(value);
+        }
+    },
+
+    locationStreetInput: {
+        get: function () {
+            return element(by.id("admin-input-location-street"));
+        },
+        set: function (value) {
+            return this.locationStreetInput.clear().sendKeys(value);
+        }
+    },
+
+    locationStreetNumberInput: {
+        get: function () {
+            return element(by.id("admin-input-location-streetNumber"));
+        },
+        set: function (value) {
+            return this.locationStreetNumberInput.clear().sendKeys(value);
+        }
+    },
+
+    companyUserUsernameInput: {
+        get: function () {
+            return element(by.id("admin-input-companyUser-username"));
+        },
+        set: function (value) {
+            return this.companyUserUsernameInput.clear().sendKeys(value);
+        }
+    },
+
+    companyUserPasswordInput: {
+        get: function () {
+            return element(by.id("admin-input-companyUser-password"));
+        },
+        set: function (value) {
+            return this.companyUserPasswordInput.clear().sendKeys(value);
+        }
+    },
+
+    companyUserEmailInput: {
+        get: function () {
+            return element(by.id("admin-input-companyUser-email"));
+        },
+        set: function (value) {
+            return this.companyUserEmailInput.clear().sendKeys(value);
+        }
+    },
+
+    companyUserNameInput: {
+        get: function () {
+            return element(by.id("admin-input-companyUser-name"));
+        },
+        set: function (value) {
+            return this.companyUserNameInput.clear().sendKeys(value);
+        }
+    },
+
+    createCompanyUserButton: {
+        get: function () {
+            return element(by.id("admin-input-createCompanyUser"));
+        }
+    },
+
+
+
+    ensureAlertifyIsNotVisible: {
+        value: function () {
+            var EC = protractor.ExpectedConditions;
+            browser.wait(EC.invisibilityOf(element(by.css(".alertify-logs"))), 5000);
+        }
+    },
+
+    ensureAddingSucceeded: {
+        value: function (typeOfUser) {
+            var EC = protractor.ExpectedConditions;
+            browser.wait(EC.visibilityOf(element(by.css(".alertify-logs"))), 5000);
+            var text = element(by.css(".alertify-logs")).element(by.css('.success')).getText();
+            return text === typeOfUser + "is created";
+        }
+    },
+
+    ensureAddingFailed: {
+        value: function (typeOfUser) {
+            var EC = protractor.ExpectedConditions;
+            browser.wait(EC.visibilityOf(element(by.css(".alertify-logs"))), 5000);
+            var text = element(by.css(".alertify-logs")).element(by.css('.error')).getText();
+            return text !== typeOfUser + "is created";
         }
     }
 });
--- src/test/protractor/e2e/users/admin/admin.spec.js
+++ src/test/protractor/e2e/users/admin/admin.spec.js
@@ -9,6 +9,10 @@ describe('Admin page', function() {
         adminPage = new AdminPage();
     });
 
+    beforeEach(function() {
+        adminPage.ensureAlertifyIsNotVisible();
+    });
+
     afterAll(function() {
         browser.executeScript('window.localStorage.clear();');
     });
@@ -17,32 +21,55 @@ describe('Admin page', function() {
 
         expect(browser.getCurrentUrl()).toEqual('http://localhost:8080/#!/admin/1');
 
-        adminPage.newUserUsernameInput = "adminko2";
+        adminPage.newUserUsernameInput = "adminko5";
         adminPage.newUserPasswordInput = "adminko";
-        adminPage.newUserEmailInput = "adminko2@com.com";
+        adminPage.newUserEmailInput = "adminko5@com.com";
         adminPage.newUserNameInput = "Aminko adminkovic";
         adminPage.newUserRoleSelect = "ADMIN";
         adminPage.createAdminVerifierButton.click();
-        //TODO try to get text from dialog;
-        expect(browser.getCurrentUrl()).toEqual('http://localhost:8080/#!/admin/1');
+
+        adminPage.ensureAddingFailed("Admin");
     });
 
     it('should successfully create new verifier', function() {
         expect(browser.getCurrentUrl()).toEqual('http://localhost:8080/#!/admin/1');
 
-        adminPage.newUserUsernameInput = "verifiarko2";
+        adminPage.newUserUsernameInput = "verifier1";
         adminPage.newUserPasswordInput = "verifiarko";
-        adminPage.newUserEmailInput = "verifiarko2@com.com";
+        adminPage.newUserEmailInput = "verifier1@com.com";
         adminPage.newUserNameInput = "verifiarko verifiarko";
         adminPage.newUserRoleSelect = "VERIFIER";
         adminPage.createAdminVerifierButton.click();
 
-        //TODO try to get text from dialog;
-        expect(browser.getCurrentUrl()).toEqual('http://localhost:8080/#!/admin/1');
+        adminPage.ensureAddingFailed("Verifier");
     });
-    //
-    // it('should successfully create new company', function() {
-    //
-    // });
+
+    it('should successfully create new company', function() {
+        expect(browser.getCurrentUrl()).toEqual('http://localhost:8080/#!/admin/1');
+
+        adminPage.companyPibInput = "12345678912";
+        adminPage.companyNameInput = "Dom AAA";
+
+        adminPage.locationCityInput = "Novi Sad";
+        adminPage.locationCityAreaInput = "Liman 3";
+        adminPage.locationZipCodeInput = 21000;
+        adminPage.locationStreetInput = "Despota Stefana";
+        adminPage.locationStreetNumberInput = "5a";
+
+        adminPage.addCompanyUserButton.click();
+
+        adminPage.companyUserUsernameInput = "slavkooo";
+        adminPage.companyUserPasswordInput = "slavkooo";
+        adminPage.companyUserEmailInput = "slavkooo@com.com";
+        adminPage.companyUserNameInput = "slavko slavko";
+
+        adminPage.createCompanyUserButton.click();
+
+        expect(adminPage.addedCompanyUserSpan.getText()).not.toEqual("");
+
+        adminPage.createCompanyButton.click();
+
+        adminPage.ensureAddingSucceeded("Company");
+    });
 
 });
